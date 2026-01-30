8157ee71cc963e4410f70304ceb9b69d79b2de1f
Christopher Glood <christopherglood@gmail.com>
2026-01-30 19:32:02 -0400

    refactor: Remove users_database module and related user creation functionality

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,8 +1,6 @@
 pub mod initialize_database;
 pub use initialize_database::initialize_database;
 
-pub mod users_database;
-
 pub mod threads;
 pub use threads::{create_thread, get_thread, update_thread, get_all_threads, delete_thread, duplicate_thread};
 
--- src-tauri/src/commands/users_database.rs
+++
@@ -1,7 +0,0 @@
-use crate::commands::initialize_database::database_conection;
-
-#[tauri::command]
-pub fn create_user(email: &str) {
-    let connection = database_conection().unwrap();
-    connection.execute("INSERT INTO users (email) VALUES (?)", [email]).unwrap();
-}
